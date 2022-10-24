7e93dfe0e5d6c2af6c94cf507a3010792f0cb0f5
jiahua2333 <ljh15088131916@gmail.com>
2022-10-24 00:09:03 -0400

    Add home page static components

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,10 +1,36 @@
 <template>
-  <div>主页</div>
+  <div>
+    <!-- 三级联动全局组件 -->
+    <TypeNav/>
+     <ListContainer/>
+     <Recommend/>
+     <Rank/>
+     <Like/>
+     <Floor/>
+     <Floor/>
+     <Brand/>
+  </div>
 </template>
 
 <script>
+
+import ListContainer from "@/pages/Home/ListContainer";
+import Recommend from "@/pages/Home/Recommend";
+import Rank from "@/pages/Home/Rank";
+import Like from "@/pages/Home/Like";
+import Floor from "@/pages/Home/Floor";
+import Brand from "@/pages/Home/Brand";
+
 export default {
-    name:'Home'
+    name:'Home',
+    components:{
+      ListContainer,
+      Recommend,
+      Rank,
+      Like,
+      Floor,
+      Brand,
+    }
 }
 </script>
 
